39bff776d1ac719163035831ec1f4e17423f70ce
MatildaMared <matildamared@live.se>
2021-07-16 16:06:31 +0200

    update styling

--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -3,24 +3,29 @@ import "./QuoteBox.scss";
 import signature from "./../../signature.png";
 
 const QuoteBox = ({ quote, getRandomQuote }) => {
-  return (
+	return (
 		<div className="wrapper">
 			<p className="description">
 				We all need to be motivated sometimes – click the button below to
-				generate a random motivational quote. Hopefully it will help brighten your day! With love,
+				generate a random motivational quote. Hopefully it will help brighten
+				your day! With love,
 			</p>
 			<img src={signature} className="signature" alt="Matilda"></img>
-			{quote ? (
-				<div className="quote">
-					<p className="quote__text">{quote.text}</p>
-          <p className="quote__author">– {quote.author}</p>
-          <button className="quote__btn" onClick={getRandomQuote}>Motivate me!</button>
-				</div>
-      ) : (
-          <div className="quote">
-            <p className="quote__loading">Loading...</p>
-          </div>
-      )}
+			<div className="quote-wrapper">
+				{quote ? (
+					<div className="quote">
+						<p className="quote__text">{quote.text}</p>
+						<p className="quote__author">– {quote.author}</p>
+						<button className="quote__btn" onClick={getRandomQuote}>
+							Motivate me!
+						</button>
+					</div>
+				) : (
+					<div className="quote">
+						<p className="quote__loading">Loading...</p>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
