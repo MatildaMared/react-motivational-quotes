a2aace8c52e740cfb22474ff10dffb8ac9ba8a77
MatildaMared <matildamared@live.se>
2021-07-16 07:49:29 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "./App.scss";
+
+import Header from "./components/Header/Header";
 import QuoteBox from "./components/QuoteBox/QuoteBox";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 	const [count, setCount] = useState(0);
 
   return <div className="App">
+    <Header />
     <QuoteBox />
+    <Footer />
   </div>;
 }
 
